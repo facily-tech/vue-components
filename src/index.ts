60c822e61c9a0b52fcd1e9499a076aa632132d8a
Tácio Sousa <tacio.oliveira@faci.ly>
2021-12-02 17:29:31 -0300

    Export component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { FyButtonInfo, FyButtonWarning, FyButtonCancel } from './components/Buttons';
+import { FyInfoCard } from './components/InfoCard';
 
 export { FyButtonInfo, FyButtonWarning, FyButtonCancel };
+export { FyInfoCard };
