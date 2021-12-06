763c5268706fc7a9a38c040f3b46a4b928d1cdba
Lucas S B Gomes <lucas.sbgomes@gmail.com>
2021-12-06 17:04:39 -0300

    feat: add modules .d.ts
    BREAKING CHANGE: add dependency

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,8 @@
-declare module 'vue-components';
+import { VueConstructor } from 'vue';
+
+export const FyButtonInfo: VueConstructor;
+export const FyButtonWarning: VueConstructor;
+export const FyButtonCancel: VueConstructor;
+export const FyNoticationBar: VueConstructor;
+export const FyInfoCard: VueConstructor;
+export const FyUserLabel: VueConstructor;
