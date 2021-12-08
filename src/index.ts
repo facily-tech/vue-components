d07d9ab8e934a58ec9f5aef883bcb3e77e2578c8
Lucas S B Gomes <lucas.sbgomes@gmail.com>
2021-12-08 10:41:15 -0300

    feat: new version 0.1.9

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,50 @@
-import { FyButtonCancel, FyButtonInfo, FyButtonWarning } from './components/Buttons';
+import {
+  FyButtonCancel,
+  FyButtonDelete,
+  FyButtonInfo,
+  FyButtonWarning,
+} from './components/Buttons';
 import { FyInfoCard } from './components/InfoCard';
 import { FyNoticationBar } from './components/NotificationBar';
 import { FyUserLabel } from './components/UserLabel';
+import { FyDataTable } from './components/DataTable';
+import {
+  FyInputCpf,
+  FyInputCnpj,
+  FyInputPhone,
+  FyInputCep,
+  FyInputDefault,
+  FyInputDate,
+  FyInputTime,
+  FyInputDateTime,
+  FyInputCurrency,
+} from './components/Inputs';
+import { FyDialog, FyDialogDelete } from './components/Dialogs';
+import { FyDrawer } from './components/Drawer';
+import { FySnackbar } from './components/Snackbar';
+import { FyToolbar } from './components/Toolbar';
 
-export { FyButtonInfo, FyButtonWarning, FyButtonCancel, FyNoticationBar };
-export { FyInfoCard };
-export { FyUserLabel };
+export {
+  FyButtonInfo,
+  FyButtonWarning,
+  FyButtonCancel,
+  FyNoticationBar,
+  FyButtonDelete,
+  FyInfoCard,
+  FyUserLabel,
+  FyDataTable,
+  FyInputCpf,
+  FyInputCnpj,
+  FyInputPhone,
+  FyInputCep,
+  FyInputDefault,
+  FyInputDate,
+  FyInputTime,
+  FyInputDateTime,
+  FyInputCurrency,
+  FyDialog,
+  FyDialogDelete,
+  FyDrawer,
+  FySnackbar,
+  FyToolbar,
+};
