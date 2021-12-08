04f6fb97259cccb92bf1be98de74aefeeee1e920
Alfredo Stanquini Neto <alfredo.neto@faci.ly>
2021-12-08 09:33:58 -0300

    feat(components): add components Inputs in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import { FyButtonCancel, FyButtonInfo, FyButtonWarning } from './components/Buttons';
 import { FyInfoCard } from './components/InfoCard';
+import {
+  FyInputCep,
+  FyInputCnpj,
+  FyInputCpf,
+  FyInputCurrency,
+  FyInputDate,
+  FyInputDateTime,
+  FyInputDefault,
+  FyInputPhone,
+  FyInputTime
+} from './components/Inputs';
 import { FyNoticationBar } from './components/NotificationBar';
 import { FyUserLabel } from './components/UserLabel';
 
 export { FyButtonInfo, FyButtonWarning, FyButtonCancel, FyNoticationBar };
 export { FyInfoCard };
 export { FyUserLabel };
+export {
+  FyInputCep,
+  FyInputCnpj,
+  FyInputCpf,
+  FyInputCurrency,
+  FyInputDate,
+  FyInputDateTime,
+  FyInputDefault,
+  FyInputPhone,
+  FyInputTime,
+};
+
