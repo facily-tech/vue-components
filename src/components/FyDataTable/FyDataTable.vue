606cdd2871130fc984096aa8cb93d7e9a3c639f1
Alfredo Stanquini Neto <alfredo.neto@faci.ly>
2022-03-02 10:41:11 -0300

    fix(datatable): adjustment import dataTable

--- src/components/FyDataTable/FyDataTable.vue
+++ src/components/FyDataTable/FyDataTable.vue
@@ -3,7 +3,7 @@
     <fy-data-table-base
       :items="items"
       :headers="headers"
-      :page.sync="page"
+      :page.sync="pageTable"
       :items-per-page="pagination ? pagination.itemsPerPage : 9999"
       :loading="loading"
       class="elevation-1 custom-datatable"
@@ -148,7 +148,7 @@
 import Vue from 'vue';
 import { VDataTable } from 'vuetify/lib';
 
-import { FyDataTableBase, FyButton } from '@/index';
+import { FyButton } from '@/index';
 
 import {
   fyFormatMaskTaxId,
@@ -171,7 +171,7 @@ const BaseDataTable = Vue.extend({ mixins: [VDataTable] });
 export default BaseDataTable.extend<BaseDataTableOptions>().extend({
   name: 'fy-data-table',
 
-  components: { FyButton, FyDataTableBase },
+  components: { FyButton, FyDataTableBase: () =>  import('./FyDataTableBase') },
 
   props: {
     headers: {
@@ -197,7 +197,7 @@ export default BaseDataTable.extend<BaseDataTableOptions>().extend({
       selectItems: [5, 10, 25, 50, 100],
       radioSelect: null as string | boolean | number | null,
 
-      page: this.pagination ? this.pagination.page : 1,
+      pageTable: this.pagination ? this.pagination.page : 1,
 
       fyFormatMaskTaxId,
       fyFormatMaskCurrency,
