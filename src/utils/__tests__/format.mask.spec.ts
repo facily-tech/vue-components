1f481949e9e67ebec1a3432452d3a187d05e2a29
Gilson Terra <gilsonterra@gmail.com>
2022-03-04 15:20:07 -0300

    test: add new tests in format.mask

--- src/utils/__tests__/format.mask.spec.ts
+++ src/utils/__tests__/format.mask.spec.ts
@@ -6,6 +6,14 @@ describe('format.mask', () => {
     expect(formatCurrencyMaskToFloat('0004', 2)).toBe(0.04);
     expect(formatCurrencyMaskToFloat('0004', 3)).toBe(0.004);
     expect(formatCurrencyMaskToFloat('USD 4000', 2)).toBe(40.0);
+
     expect(formatCurrencyMaskToFloat('R$ 1234', 2)).toBe(12.34);
+    expect(formatCurrencyMaskToFloat('R$ 12,34', 2)).toBe(12.34);
+
+    expect(formatCurrencyMaskToFloat('R$ 123400', 2)).toBe(1234.0);
+    expect(formatCurrencyMaskToFloat('R$ 1234,00', 2)).toBe(1234.0);
+    expect(formatCurrencyMaskToFloat('R$ 1234.00', 2)).toBe(1234.0);
+
+    expect(formatCurrencyMaskToFloat('R$ 1234.00', 3)).toBe(123.4);
   });
 });
