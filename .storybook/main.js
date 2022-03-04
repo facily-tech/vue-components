15c811977488e4208d02e06b7c9e571a0e1f972d
Gilson Terra <gilsonterra@gmail.com>
2022-03-04 10:10:00 -0300

    feat: add new config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,11 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/addon-controls',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@socheatsok78/storybook-addon-vuetify',
+    'storybook-addon-turbo-build',
   ],
   framework: '@storybook/vue',
   webpackFinal: async (config, { configType }) => {
