4abaddf9e28288603c45379a2c0e0686d07f3b17
felipebrasil8facily <felipe.brasil@facily>
2022-03-29 15:08:29 -0300

    fix: add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: [
+    '<rootDir>/src/**/*.(spec|test).{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  collectCoverage: true,
   testResultsProcessor: 'jest-sonar-reporter',
 };
