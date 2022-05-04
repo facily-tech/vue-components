8af24048a92d31a6a36a32a0bb83cab5a78951a2
Gilson Terra <gilsonterra@gmail.com>
2022-05-04 15:14:08 -0300

    fix: autocomplete item slot

--- src/components/FyAutocomplete/FyAutocomplete.vue
+++ src/components/FyAutocomplete/FyAutocomplete.vue
@@ -2,7 +2,17 @@
   <v-autocomplete dense outlined :no-data-text="noData" v-bind="$attrs" v-on="$listeners">
     <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
     <template #item="{ item }">
-      <slot name="item-data" v-bind="item" />
+      <slot
+        name="item-data"
+        v-if="!!$slots['item-data'] || !!$scopedSlots['item-data']"
+        v-bind="item"
+      />
+      <v-list-item-content v-else-if="!!$attrs['item-text']">
+        {{ item[$attrs['item-text']] }}
+      </v-list-item-content>
+      <v-list-item-content v-else>
+        {{ item }}
+      </v-list-item-content>
     </template>
   </v-autocomplete>
 </template>
