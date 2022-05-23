8435641fbc068c0499f7a016ef21d4c3912147f1
Gilson Terra <gilsonterra@gmail.com>
2022-05-23 12:08:56 -0300

    feat: add new cpf validation

--- src/components/FyInput/FyInputCpf.vue
+++ src/components/FyInput/FyInputCpf.vue
@@ -16,6 +16,44 @@ import Vue from 'vue';
 import { VueMaskDirective } from 'v-mask';
 import FyInput from './FyInput.vue';
 
+const isValidCPF = (cpf: string): boolean => {
+  const cpfOnlyNumbers = cpf.replace(/[^\d]+/g, '');
+
+  if (
+    [
+      '00000000000',
+      '11111111111',
+      '22222222222',
+      '33333333333',
+      '44444444444',
+      '55555555555',
+      '66666666666',
+      '77777777777',
+      '88888888888',
+      '99999999999',
+    ].includes(cpfOnlyNumbers)
+  ) {
+    return false;
+  }
+
+  if (cpfOnlyNumbers.length !== 11 || !!cpfOnlyNumbers.match(/(\d)\1{10}/)) {
+    return false;
+  }
+
+  const cpfArray = cpfOnlyNumbers.split('');
+  const validator = cpfArray
+    .filter((digit, index, array) => index >= array.length - 2 && digit)
+    .map((el) => +el);
+
+  const toValidate = (pop: number) =>
+    cpfArray.filter((digit, index, array) => index < array.length - pop && digit).map((el) => +el);
+
+  const rest = (count: number, pop: number) =>
+    ((toValidate(pop).reduce((soma, el, i) => soma + el * (count - i), 0) * 10) % 11) % 10;
+
+  return !(rest(10, 2) !== validator[0] || rest(11, 1) !== validator[1]);
+};
+
 export default Vue.extend({
   name: 'fy-input-cpf',
 
@@ -34,10 +72,7 @@ export default Vue.extend({
   data() {
     return {
       text: this.value ? this.value : '',
-      cpfRule: [
-        (value: string): boolean | string =>
-          value.length === 14 || 'O campo Cpf deve conter 11 números',
-      ],
+      cpfRule: [(value: string): boolean | string => isValidCPF(value) || 'O CPF deve ser válido'],
       mask: '###.###.###-##',
     };
   },
